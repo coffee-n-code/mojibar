553cdfc3a55c6691aca06cfba6a874be6c82f2fa
hamizmz <root@graham-robertson.ca>
2016-10-15 18:28:55 -0400

    Initial Fork 🍔

--- index.js
+++ index.js
@@ -1,108 +1,31 @@
-var menubar = require('menubar')
-var ipc = require('electron').ipcMain
-var globalShortcut = require('electron').globalShortcut
-var mb = menubar({ dir: __dirname + '/app', width: 440, height: 230, icon: __dirname + '/app/Icon-Template.png', preloadWindow: true, windowPosition: 'topRight' })
-var Menu = require('electron').Menu
+'use strict';
 
-mb.on('show', function () {
-  mb.window.webContents.send('show')
-})
+const electron = require('electron');
+const app = electron.app;
+const WIDTH = 460;
+const HEIGHT = 540;
 
-mb.app.on('will-quit', function () {
-  globalShortcut.unregisterAll()
-})
+var _window = null;
 
-mb.app.on('activate', function () {
-  mb.showWindow()
-})
+/*
+	WINDOW JUNK
+*/
+app.on('window-all-closed', function() {
+	// On OS X, we don't quit the program when all windows are closed (usually)
+	if (process.platform != 'darwin')
+		app.quit();
+});
 
-// when receive the abort message, close the app
-ipc.on('abort', function () {
-  mb.hideWindow()
-})
-
-// when receive the abort message, close the app
-ipc.on('update-preference', function (evt, pref, initialization) {
-  registerShortcut(pref['open-window-shortcut'], initialization)
-})
-
-var template = [
-  {
-    label: 'Mojibar',
-    submenu: [
-      {
-        label: 'Undo',
-        accelerator: 'Command+Z',
-        selector: 'undo:'
-      },
-      {
-        label: 'Redo',
-        accelerator: 'Shift+Command+Z',
-        selector: 'redo:'
-      },
-      {
-        label: 'Cut',
-        accelerator: 'Command+X',
-        selector: 'cut:'
-      },
-      {
-        label: 'Copy',
-        accelerator: 'Command+C',
-        selector: 'copy:'
-      },
-      {
-        label: 'Paste',
-        accelerator: 'Command+V',
-        selector: 'paste:'
-      },
-      {
-        label: 'Select All',
-        accelerator: 'Command+A',
-        selector: 'selectAll:'
-      },
-      {
-        label: 'Reload',
-        accelerator: 'CmdOrCtrl+R',
-        click: function (item, focusedWindow) { if (focusedWindow) focusedWindow.reload() }
-      },
-      {
-        label: 'Preference',
-        accelerator: 'Command+,',
-        click: function () { mb.window.webContents.send('open-preference') }
-      },
-      {
-        label: 'Quit App',
-        accelerator: 'Command+Q',
-        selector: 'terminate:'
-      },
-      {
-        label: 'Toggle DevTools',
-        accelerator: 'Alt+Command+I',
-        click: function () { mb.window.toggleDevTools() }
-      }
-    ]
-  }
-]
-
-mb.on('ready', function ready () {
-  // Build default menu for text editing and devtools. (gone since electron 0.25.2)
-  var menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
-})
-
-// Register a shortcut listener.
-var registerShortcut = function (keybinding, initialization) {
-  globalShortcut.unregisterAll()
-
-  try {
-    var ret = globalShortcut.register(keybinding, function () {
-      mb.window.isVisible() ? mb.hideWindow() : mb.showWindow()
-    })
-  } catch (err) {
-    mb.window.webContents.send('preference-updated', false, initialization)
-  }
-
-  if (ret) {
-    mb.window.webContents.send('preference-updated', true, initialization)
-  }
-}
+app.on('ready', function() {
+	_window = new electron.BrowserWindow({
+		"width": WIDTH,
+		"height": HEIGHT
+	});
+	_window.setMenuBarVisibility(false);
+	_window.loadURL('file://' + __dirname + '/app/index.html');
+	
+	// CLEAN-UP Window
+	_window.on('closed', function() {
+		_window = null;
+	});
+});
